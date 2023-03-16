972625cfea1ba1f8594596941ebf199aa931e04a
abdellatif <buss502179@gmail.com>
2023-03-16 14:59:50 -0700

    last

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+/**
+ * * main -Entry point
+ * *
+ * * Reurn: Always 0 (Success)
+ * */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/***
+ * main -Entry point
+ * *
+ * * Return: always 0 (Success)
+ * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
