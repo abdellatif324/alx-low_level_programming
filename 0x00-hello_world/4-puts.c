ae6819db0d5de30ca3381e711dd8b628fc3331d8
abdellatif <buss502179@gmail.com>
2023-03-16 14:23:39 -0700

    last easy task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
-#!/bin/bash
-#include<stdio.h>()
+#include<stdio.h>
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n);
+	puts("\"Programming is like building a multilingual puzzle ");
+	  		return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (void)
+	{
+		printf("size of a char:%d byte(s)\n",siezof(char));
+	        printf("size of an int :%d byte(s)\n"),sizeof(int));
+	 	printf("size of a long int:%d byte(s)"),sizeof(long int));
+	 	printf("Size of a float:%d byte(s)"),sizeof(float));
+                           return (0);
+	}
