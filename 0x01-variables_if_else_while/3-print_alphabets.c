25ffa7ce1982e94d0ffd80d66195459050452481
abdellatif <buss502179@gmail.com>
2023-03-17 14:07:48 -0700

    4dr

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*	main - prints the alphabet
+*
+*	Return: Always  (Success)
+**/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}	
+	 putchar('\n');
+		return (0);
+}
