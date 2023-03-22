d225b61760bd0583337801a7320ef4771e14b034
abdellatif <buss502179@gmail.com>
2023-03-21 18:14:05 -0700

    last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,18 +5,14 @@
  * Return: value of the last digit
  */
 int print_last_digit(int n)
-{ 
-	int a ; 
+{
+	int a;
+
 	if (n < 0)
 	n = -n;
-	
-		a = n % 10 ;
-		
+	a = n % 10;
 		if (a < 0)
-			a = -a;
-		
-		_putchar(a + '0'); 
+		a = -a;
+		_putchar(a + '0');
 		return (a);
-			
-	
 }
