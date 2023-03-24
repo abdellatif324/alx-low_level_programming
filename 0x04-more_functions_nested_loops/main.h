c24976d7c66e34f260a02c16e9d278b69564d4fa
abdellatif <buss502179@gmail.com>
2023-03-23 19:21:08 -0700

    last

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
@@ -10,3 +13,5 @@ int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_number(int n);
+#endif
