3681b23564019cb78db7ba5fd90ab46a103cde10
abdellatif <buss502179@gmail.com>
2023-03-23 18:20:51 -0700

    easy

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers 0-9 - did not print 2 and 4
+ * return: void
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if(c != '2' && c != '4')
+		{
+		_putchar(c);
+		}
+	}
+		_putchar('\n');
+}
