cf6a76fcaf491bf18198e9b5bf07a973734f58e1
abdellatif <buss502179@gmail.com>
2023-03-30 19:19:58 -0700

    klk

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,3 +14,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
