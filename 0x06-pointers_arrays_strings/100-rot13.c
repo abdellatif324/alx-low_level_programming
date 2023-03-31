973373a1d71c8ad5104e6637cb9247089840fb01
abdellatif <buss502179@gmail.com>
2023-03-30 18:59:41 -0700

    all

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int a;
+	int b;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b < 52; b++)
+		{
+			if (s[a] == data1[b])
+			{
+				s[a] = datarot[b];
+				break;
+			}
+		}
+	}
+	return (s);
+}
