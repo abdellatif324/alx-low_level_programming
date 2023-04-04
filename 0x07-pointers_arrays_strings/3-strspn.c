88f2a6a8ca64b089819d7bed6dd63cea0bf5836e
abdellatif <buss502179@gmail.com>
2023-04-03 17:29:02 -0700

    m

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int n = 0;
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				n++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
