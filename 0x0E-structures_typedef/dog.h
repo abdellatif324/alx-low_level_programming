238042435ad631d8c02661a663dd9c16bc9c6cc0
abdellatif <buss502179@gmail.com>
2023-04-17 13:06:07 -0700

    lmlm

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,6 @@ struct dog
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
