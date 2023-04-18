075552b879a749e9e512152a14f3ca42e0dce020
abdellatif <buss502179@gmail.com>
2023-04-17 17:43:15 -0700

    lm

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+julien@ubuntu:~/0x0c. macro, structures$ cat 4-main.c
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = SUM(98, 1024);
+    printf("%d\n", s);
+    return (0);
+}
