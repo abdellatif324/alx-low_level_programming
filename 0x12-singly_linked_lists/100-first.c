03533c8e9ca20cace22604d3e9ef158ba71ca564
abdellatif <buss502179@gmail.com>
2023-04-27 16:48:25 -0700

    last

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * first - function that prints befor the main
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
