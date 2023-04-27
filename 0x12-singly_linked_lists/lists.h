9a535e286c15746b6b0e0a74006950b95eaaf6d1
abdellatif <buss502179@gmail.com>
2023-04-27 14:14:35 -0700

    first

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-
+/**
+ * struct list_s - linked list
+ * @str: string of string
+ * @len: lenght
+ * @next: pointe to the next node
+ */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 
 
 
