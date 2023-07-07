63681e9be765b0f0eb7e4dfc92fa24f3e6ae6545
abdellatif324 <buss502179@gmail.com>
2023-07-06 17:14:05 -0700

    mlml

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * clear_bit -  that returns the number of bits
+ * @n: calculated using the function
+ * @m: calculated using the function
+ * Return: count
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor;
+	unsigned int count = 0;
+
+	xor = n ^ m;
+
+	while (xor != 0)
+	{
+		xor &= (xor - 1);
+		count++;
+	}
+
+	return (count);
+}
