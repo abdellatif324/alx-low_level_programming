ce9d4259a3d6668deac61b68a8a514724cad7dc5
abdellatif324 <buss502179@gmail.com>
2023-07-10 15:15:15 -0700

    mlm

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - that reads text file and prints
+ * @filename: filename
+ * @letters: numbers letters
+ *
+ * Return: Total numbers letters printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t total, bytes;
+	char *buffer;
+	int a;
+
+	if (filename == NULL)
+		return (0);
+
+	a = open(filename, O_RDONLY);
+
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+
+	if (buffer == NULL)
+		return (0);
+
+	bytes = read(a, buffer, letters);
+	total = write(STDOUT_FILENO, buffer, bytes);
+
+	close(a);
+	free(buffer);
+
+	return (total);
+}
+
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_h
+#define MAIN_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+#endif
