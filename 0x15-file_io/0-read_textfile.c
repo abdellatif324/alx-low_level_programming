4c04dc9655ac0cc078cd7c4e042b0c991843d423
abdellatif324 <buss502179@gmail.com>
2023-07-11 04:24:34 -0700

    mlm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (a == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters + 1));
+	buffer = malloc(sizeof(char) * (letters));
 
 	if (buffer == NULL)
 		return (0);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,16 @@
 #ifndef MAIN_h
 #define MAIN_H
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <stddef.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
-ssize_t read_textfile(const char *filename, size_t letters);
 
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 
 
