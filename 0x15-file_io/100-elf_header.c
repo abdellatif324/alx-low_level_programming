d851c43cd502b785f90bc2f9bad8615f5b6e19f3
abdellatif324 <buss502179@gmail.com>
2023-07-11 15:45:27 -0700

    mlmllm

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,18 +15,17 @@ void print_elf_header(int fd)
 {
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
-    int i;
 
-    /* Read ELF header */
+    // Read ELF header
     bytes_read = read(fd, buffer, BUFFER_SIZE);
     if (bytes_read == -1)
         error_exit(98, "Error: Failed to read ELF header");
 
-    /* Print ELF header information */
+    // Print ELF header information
     printf("ELF Header:\n");
     printf("  Magic:   ");
-    for (i = 0; i < 16; i++)
-        printf("%02x ", (unsigned char)buffer[i]);
+    for (int i = 0; i < 16; i++)
+        printf("%02x ", buffer[i]);
     printf("\n");
     printf("  Class:                             ELF%d\n", buffer[4] == 1 ? 32 : 64);
     printf("  Data:                              %s\n", buffer[5] == 1 ? "2's complement, little endian" : "2's complement, big endian");
@@ -49,8 +48,8 @@ void print_elf_header(int fd)
             printf("<unknown>\n");
     }
     printf("  Entry point address:               0x");
-    for (i = 0; i < 8; i++)
-        printf("%02x", (unsigned char)buffer[24 + i]);
+    for (int i = 0; i < 8; i++)
+        printf("%02x", buffer[24 + i]);
     printf("\n");
 }
 
@@ -61,17 +60,18 @@ int main(int argc, char **argv)
     if (argc != 2)
         error_exit(97, "Usage: elf_header elf_filename");
 
-    /* Open the ELF file */
+    // Open the ELF file
     fd = open(argv[1], O_RDONLY);
     if (fd == -1)
         error_exit(98, "Error: Failed to open ELF file");
 
-    /* Print ELF header */
+    // Print ELF header
     print_elf_header(fd);
 
-    /* Close the file */
+    // Close the file
     if (close(fd) == -1)
         error_exit(100, "Error: Failed to close file");
 
     return 0;
 }
+
