9997d35f7c636a43d4e467a67f2213bc3ef1197f
abdellatif324 <buss502179@gmail.com>
2023-07-11 05:40:46 -0700

    mlmllm

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor;
-	ssize_t number, bytes_read;
+	int number, bytes_read;
 
 	if (filename == NULL)
 		return (-1);
