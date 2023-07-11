4a8a27c79a7e6be8f4465a0a9e17736f92346382
abdellatif324 <buss502179@gmail.com>
2023-07-11 05:39:37 -0700

    mlml

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * creat_file -  that creates a file.
+ * create_file - function that creates a file..
  * @filename: filename
  * @text_content: content writed
  *
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 
 
@@ -13,27 +13,27 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor;
-	ssize_t bytes_written;
+	ssize_t number, bytes_read;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (file_descriptor == -1)
 		return (-1);
 
-		if (text_content != NULL)
-		{
-		bytes_written = write(file_descriptor, text_content, strlen(text_content));
+	if (!text_content)
+		text_content = "";
+	for (number = 0; text_content[number]; number++)
+		;
 
-			if (bytes_written == -1)
-			{
-				close(file_descriptor);
-				return (-1);
-			}
-	}
+
+	bytes_read = read(file_descriptor, text_content, number);
+
+	if (bytes_read == -1)
+		return (-1);
 
 	close(file_descriptor);
-
 	return (1);
 }
