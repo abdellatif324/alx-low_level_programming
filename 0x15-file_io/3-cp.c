d0293fac72c2ee293710a7d13b43ffbff418a8a0
abdellatif324 <buss502179@gmail.com>
2023-07-11 14:46:57 -0700

    mlmlmlml

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,38 +5,41 @@
 
 #define BUFFER_SIZE 1024
 
-void error_exit(int code, const char *message) {
-    dprintf(STDERR_FILENO, "%s\n", message);
-    exit(code);
+void error_exit(int code, const char *message)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(code);
 }
 
-int main(int argc, char **argv) {
-    if (argc != 3)
-        error_exit(97, "Usage: cp file_from file_to");
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
 
-    int fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-        error_exit(98, "Error: Can't read from file");
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to");
 
-    int fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd_to == -1)
-        error_exit(99, "Error: Can't write to file");
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file");
 
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read, bytes_written;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to file");
 
-    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written == -1)
-            error_exit(99, "Error: Can't write to file");
-    }
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+			error_exit(99, "Error: Can't write to file");
+	}
 
-    if (bytes_read == -1)
-        error_exit(98, "Error: Can't read from file");
+	if (bytes_read == -1)
+		error_exit(98, "Error: Can't read from file");
 
-    if (close(fd_from) == -1 || close(fd_to) == -1)
-        error_exit(100, "Error: Can't close fd");
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd");
 
-    return 0;
+	return (0);
 }
-
