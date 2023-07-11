7426d55740e6b31db63b71c4438a22b0247fd7e5
abdellatif324 <buss502179@gmail.com>
2023-07-11 05:58:55 -0700

    lml

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,27 +12,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	int number, bytes_read;
+	int file_descriptor, bytes_read;
+	int number = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (file_descriptor == -1)
-		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (number = 0; text_content[number]; number++)
-		;
+	if (text_content != NULL)
+	{
+		for (number = 0; text_content[number];)
+			number++;
+	}
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes_read = write(file_descriptor, text_content, number);
 
 
-	bytes_read = read(file_descriptor, text_content, number);
-
-	if (bytes_read == -1)
+	if (file_descriptor == -1 || bytes_read == -1)
 		return (-1);
 
 	close(file_descriptor);
