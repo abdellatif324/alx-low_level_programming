4a00df090aed987bb06a54d9334a4f7458d3ac2a
abdellatif324 <buss502179@gmail.com>
2023-08-03 16:15:06 -0700

    task 2

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ * @c: The character to be written.
+ *
+ * Return: On success, the character written is returned.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
 
 unsigned int binary_to_uint(const char *b);
 
+void print_binary(unsigned long int n);
 
 
 #endif
