b681ceafd23e585101f4b8af5a06ab2cde208416
abdellatif324 <buss502179@gmail.com>
2023-08-03 16:51:15 -0700

    all

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of a bit to 1 at a given index.
+ * @n: A pointer to the number to modify.
+ * @index: The index of the bit to set, starting from 0.
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n |= (1UL << index);
+	return (1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns the number of bits to flip to convert one number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
+
+	while (xor_result > 0)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+
+	return (count);
+}
+
