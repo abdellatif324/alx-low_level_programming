5a5195685336d7733378f5310f05bc3aaa3bdedd
abdellatif324 <buss502179@gmail.com>
2023-08-07 15:40:34 -0700

    lmml

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,71 +4,108 @@
 #include <unistd.h>
 
 /**
- * main - Copies the content of a file to another file.
+ * check_arguments - Checks the number of arguments.
  * @argc: Number of arguments.
  * @argv: Arguments vector.
- * Return: 0 on success, exit codes on failure.
+ *
+ * Return: No return value. Exits if the number of arguments is incorrect.
  */
-int main(int argc, char *argv[])
+void check_arguments(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars;
-	char buf[1024];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
+}
 
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+/**
+ * open_file - Opens a file with specified flags and mode.
+ * @filename: Name of the file to open.
+ * @flags: Flags for opening the file.
+ * @mode: Mode for file permissions (only applicable when creating a file).
+ *
+ * Return: File descriptor of the opened file. Exits on error.
+ */
+int open_file(const char *filename, int flags, mode_t mode)
+{
+	int fd = open(filename, flags, mode);
+
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
+	return (fd);
+}
 
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+/**
+ * copy_file - Copies content from source file to destination file.
+ * @source_fd: File descriptor of the source file.
+ * @dest_fd: File descriptor of the destination file.
+ *
+ * Return: No return value. Exits on read or write errors.
+ */
+void copy_file(int source_fd, int dest_fd)
+{
+	ssize_t nchars;
+	char buf[1024];
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
+		nchars = read(source_fd, buf, 1024);
 		if (nchars == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close(file_from);
-			close(file_to);
+			dprintf(STDERR_FILENO, "Error: Can't read from file\n");
 			exit(98);
 		}
-		if (write(file_to, buf, nchars) == -1)
+		if (write(dest_fd, buf, nchars) == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(file_from);
-			close(file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
 			exit(99);
 		}
 	}
+}
 
-	err_close = close(file_from);
+/**
+ * close_file - Closes a file descriptor.
+ * @fd: File descriptor to close.
+ * @filename: Name of the file associated with the file descriptor.
+ *
+ * Return: No return value. Exits on error.
+ */
+void close_file(int fd, const char *filename)
+{
+	int err_close = close(fd);
+
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd for %s\n", filename);
 		exit(100);
 	}
+}
 
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+/**
+ * main - Entry point of the program.
+ * @argc: Number of arguments.
+ * @argv: Arguments vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
+
+	check_arguments(argc, argv);
+
+	file_from = open_file(argv[1], O_RDONLY, 0);
+	file_to = open_file(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+
+	copy_file(file_from, file_to);
+
+	close_file(file_from, argv[1]);
+	close_file(file_to, argv[2]);
 
 	return (0);
 }
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,4 +7,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void check_arguments(int argc, char *argv[]);
+int open_file(const char *filename, int flags, mode_t mode);
+void copy_file(int source_fd, int dest_fd);
+void close_file(int fd, const char *filename);
+
 #endif
